cf609c95b77eae4a64ba34ec296ba61fc0866324
Taylor <taylor.larson5@gmail.com>
2025-10-03 22:00:01 -0500

    Create Xcode project structure for TestFlight
    - Move source files to proper Xcode project structure
    - Create MyContactPoint.xcodeproj with proper configuration
    - Organize files in MyContactPoint/ subfolder
    - Ready for archive and TestFlight distribution

---
+++ MyContactPoint/MyContactPoint/MyContactPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyContactPointApp.swift
+//  MyContactPoint
+//
+//  Created by Taylor Larson on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MyContactPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
